c0bfa6384a078321f6b7a8e47f1e255550afe9bb
Prateeshtalasu <prateeshtalasu1999@gmail.com>
2020-07-13 16:18:09 +0530

    intial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E1
+{
+    class Avgof3
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, num3; 
+            double res;
+            Console.WriteLine("enter three numbers");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            num3 = Convert.ToInt32(Console.ReadLine());
+            res = (num1 + num2 + num3) / 3;
+            Console.WriteLine("the avg is :" + res);
+
+        }
+    }
+}
